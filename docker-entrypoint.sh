ce03a2333ba683bbf0d5ffe909cb26bae6818609
Vinicius S <vinicius@Viniciuss-MacBook-Air.local>
2015-04-04 19:24:23 -0300

    alteracao de gitignore e database.yml

--- docker-entrypoint.sh
+++
@@ -1,1 +0,0 @@
-echo "Docker was built completely."
--- script.sh
+++
@@ -1,9 +0,0 @@
-export APP_PWD=password
-
-db() {
-	docker run -P --volumes-from app_data --name app_db -e POSTGRES_USER=app_user -e POSTGRES_PASSWORD=$APP_PWD -d -t postgres:latest
-}
-
-action=$1
-
-${action}
